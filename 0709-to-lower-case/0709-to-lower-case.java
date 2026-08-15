61c64f0d7ee243f70f1f9633ee480c11d1cc20a6
Bapurao Jadhav <159645638+Bapurao-jadhav@users.noreply.github.com>
2026-08-15 16:39:45 +0530

    Time: 1 ms (24.06%), Space: 42.9 MB (68.15%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,13 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        String str="";
+        StringBuilder str=new StringBuilder();
         for(int i =0; i<s.length(); i++){
             char ch= s.charAt(i);
             if(ch>=65 && ch <=90){
                 ch+=32;
             }
-            str+=ch;
+            str.append(ch);
         }
-        return str;
+        return str.toString();
     }
 }
