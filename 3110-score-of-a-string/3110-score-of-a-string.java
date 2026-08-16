77ce6cc1ae4bb57d768f444e7ca4b7d0c16fdb0f
Bapurao Jadhav <159645638+Bapurao-jadhav@users.noreply.github.com>
2026-08-16 18:51:21 +0530

    Time: 1 ms (98.27%), Space: 43.5 MB (37%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,14 +1,10 @@
 class Solution {
     public int scoreOfString(String s) {
-        int i =0;
-        int j =i+1;
+        int i =1;
         int res=0;
-        while (j<s.length()){
-            char ch1= s.charAt(i);
-            char ch2 = s.charAt(j);
-            res= res + Math.abs(ch1-ch2);
+        while (i<s.length()){
+            res= res + Math.abs(s.charAt(i)-s.charAt(i-1));
             i++;
-            j++;
         }
         return res;
     }
